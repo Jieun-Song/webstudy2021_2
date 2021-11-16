6b34e757647556cd0b231bdea054d8a45cf23787
jieun song <80374808+Jieun-Song@users.noreply.github.com>
2021-11-16 10:54:02 +0900

    20211116

--- 202111/variable.js
+++ 202111/variable.js
@@ -38,4 +38,40 @@ const daysInWeek = 7;
 const maxNumber = 5;
 
 //4. Variable types
-//
+//first-class function(변수에 함수를 할당 가능)
+//선언x 숫자는 number만 사용하면 됨.
+let a = 12;//정수
+let b = 1.2;//소숫점의 숫자
+
+const infinity = 1/0;//0으로 나누면 무한
+const negativeInfinity = -1/0; 
+const nAn = 'not a number'/2;
+
+//bigInt
+const bigInt = 123456789012345678901234567890n; //새롭게 추가된것
+
+//string 모두 스트링타입으로 정의됨
+const char = 'c';
+const brendan = 'brandan';
+const greeting = 'hello' + brendan;
+//mutable
+
+//boolean
+//false = 0,null,nudefined, NaN, ''
+
+//null 
+let nothing = null;
+//텅텅비어있다
+
+//undefined
+let x;
+//비어있는지 아닌지 아직 정해지지 않았다.
+
+//symbol, xreate unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);//false
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1===gSymbol2); //true
+
